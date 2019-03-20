6b6a2ec3127db5e0b4e45638d53f827c365eaf06
Affee <m13120158558@163.com>
2019-03-20 17:45:30 +0800

    Main

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import Main from './js/page/Main'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,9 +22,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>欢迎来到英雄联盟</Text>
-        <Text style={styles.instructions}>欢迎来到德莱联盟 aaa</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+          <Main/>
       </View>
     );
   }
@@ -40,10 +39,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
